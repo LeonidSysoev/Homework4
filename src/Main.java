8ff1688876bc2b2a53f013743ef4ef2022060890
LeonidHome <defenser2205@gmail.com>
2023-01-17 19:00:34 +0300

    task 4-5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
 
     public static void task1() {
@@ -37,11 +39,74 @@ public class Main {
 
     public static void task3() {
         System.out.println("Задача 3");
-        int year = 2017;
+        int year = 2024;
         if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
             System.out.println(year + " год является високосным");
-        }else {
+        } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 80;
+        int deliveryDays = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("На доставку потребуется " + deliveryDays + " день");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            deliveryDays++;
+            System.out.println("На доставку потребуется " + deliveryDays + " дня");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryDays += 2;
+            System.out.println("На доставку потребуется " + deliveryDays + " дня");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставка не осуществляется");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь, сезон зима");
+                break;
+            case 2:
+                System.out.println("Февраль, сезон зима");
+                break;
+            case 3:
+                System.out.println("Март, сезон весна");
+                break;
+            case 4:
+                System.out.println("Апрель, сезон весна");
+                break;
+            case 5:
+                System.out.println("Май, сезон весна");
+                break;
+            case 6:
+                System.out.println("Июнь, сезон лето");
+                break;
+            case 7:
+                System.out.println("Июль, сезон лето");
+                break;
+            case 8:
+                System.out.println("Август, сезон лето");
+                break;
+            case 9:
+                System.out.println("Сентябрь, сезон осень");
+                break;
+            case 10:
+                System.out.println("Октябрь, сезон осень");
+                break;
+            case 11:
+                System.out.println("Ноябрь, сезон осень");
+                break;
+            case 12:
+                System.out.println("Декабрь, сезон зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует.");
+
         }
     }
 }
